71845f6fee4a218682b61bf85248575f453b5072
Jonas Rogert <jonas.rogert@britny.se>
2015-12-08 17:43:36 +0100

    changing to the objects manager when using multisite setup

--- djangocms_blog/views.py
+++ djangocms_blog/views.py
@@ -16,7 +16,7 @@ class BaseBlogView(ViewUrlMixin):
 
     def get_queryset(self):
         language = get_language()
-        queryset = self.model._default_manager.active_translations(language_code=language)
+        queryset = self.model.objects.active_translations(language_code=language)
         if not getattr(self.request, 'toolbar', False) or not self.request.toolbar.edit_mode:
             queryset = queryset.published()
         return queryset.on_site()
